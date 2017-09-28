54cc6bed5ac5a1c8aeb7f39d0adef2fa7e17a28e
André Andrade <ardy.aileron@gmail.com>
2017-09-28 12:40:41 -0300

    global stuff
    cuz I'd better name thing properly
    > esc to quit

--- scripts/global.gd
+++ scripts/global.gd
@@ -1,5 +1,4 @@
 extends Node
-
 var a = (256*4)
 var b = (224*4)
 
@@ -13,4 +12,8 @@ func _spaceBg():
 func _ready():
 	_get_big()
 	_spaceBg()
-    
+	set_process_input(true)
+
+func _input(event):
+	if event.is_action_pressed("escape"):
+		get_tree().quit()
