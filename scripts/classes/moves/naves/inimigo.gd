6688c2c22453ad52d655755218246c45b40eceb5
André Andrade <ardy.aileron@gmail.com>
2017-09-29 19:36:39 -0300

    default enemy

--- scripts/classes/moves/naves/inimigo.gd
+++ scripts/classes/moves/naves/inimigo.gd
@@ -1,10 +1,5 @@
-extends Node
-
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
+extends "res://scripts/classes/moves/naves.gd"
 
 func _ready():
-	# Called every time the node is added to the scene.
-	# Initialization here
-	pass
+	dir =  -1
+	shootAuto = false
