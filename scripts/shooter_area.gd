03fe032cee4978366eff92b32c3ad7a033f6279c
André Andrade <ardy.aileron@gmail.com>
2017-09-30 17:16:01 -0300

    some cleanup I guess

--- scripts/shooter_area.gd
+++ scripts/shooter_area.gd
@@ -5,7 +5,7 @@ func collides(thing):
 	if "shooter" in thing:
 		if thing.shooter:
 			if "go" in thing:
-				print(thing.get_name())
+				# print(thing.get_name())
 				thing.go = true
 
 func _ready():
