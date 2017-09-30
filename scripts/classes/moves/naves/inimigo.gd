bdfa6cca2f744af4c36ac268b45ccf9a07b75327
André Andrade <ardy.aileron@gmail.com>
2017-09-30 14:03:36 -0300

    follow and quick follow implementation

--- scripts/classes/moves/naves/inimigo.gd
+++ scripts/classes/moves/naves/inimigo.gd
@@ -4,19 +4,33 @@ var go = false
 # guard vars
 var guard = false
 var follow = false
+var followQuick = false
 var followSpeed = 0.5
 onready var followTarget = get_node("../stage/octopedro")
 
 # shooter vars
 var shooter = false
 
-func _fixed_process():
+func _fixed_process(delta):
 	if follow:
-		print("ahoy!")
-		var a = followTarget.get_pos().y
-		var b = get_pos().y
-		var followDir = Vector2(0,b - a)
-		set_pos(followDir * followSpeed)
+		var pos = get_pos()
+		var curY = pos.y
+		var tarY = followTarget.get_pos().y
+		var followDir = Vector2()
+		
+		if followQuick:
+			follow = false
+			followDir = Vector2(0,tarY - curY)
+			set_pos(get_pos() + followDir * followSpeed)
+			pass
+		else:
+			if tarY < curY:
+				followDir = Vector2(0, -1)
+			if tarY > curY:
+				followDir = Vector2(0, 1)
+			if tarY == curY:
+				followDir = Vector2(0, 0)
+			set_pos(pos + followDir * followSpeed)
 
 func _ready():
 	set_process(true)
