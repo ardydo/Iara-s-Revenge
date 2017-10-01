c556be627f208ce1a61a971e0b3f6af895d457f9
André Andrade <ardy.aileron@gmail.com>
2017-09-30 23:41:48 -0300

    intro update

--- scripts/intro.gd
+++ scripts/intro.gd
@@ -2,6 +2,6 @@ extends Label
 var intro = ""
 
 func _ready():
-	intro = "Depois de ter seu castelo destruído, Octopedro fugiu para a praia mas as criaturas do mar ainda o perseguiam mandando onda após onda para tentar trazê-lo de volta às profundezas do mar. Enquanto pulava, pensava em quantos favores iria cobrar de Iemanjá depois de tantas ondas. Ofendida com o desrespeito de Octopedro a própria Iemanjá manda uma onda gigante, grande demais para Octopedro pular. Entretanto, a onda foi tão grande que acabou por levar Octopedro ao espaço. Iemanjá ordena que as criaturas mitológicas vão dar uma lição em Octopedro."
+	intro = "Depois de ter seu castelo destruído, Octopedro fugiu para a praia mas as criaturas do mar ainda o perseguiam mandando onda após onda para tentar trazê-lo de volta às profundezas do mar. Depois de pular tantas e tantas ondas, Octopedro chega ao espaço. Entretanto, as ondas levaram o lixo para o mar para os rios. A protetora das águas, Iara, sabendo da inocência dos seres marinhos ordena que as criaturas mitológicas e Marcos Pasquim dêem uma lição em Octopedro."
 	set_text(intro)
 	global._scene_changer("res://title.tscn")
